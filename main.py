ac5a909ed58d55a9ae4798f143efcd2af7645f8f
Alan Li <alan.li0097@gmail.com>
2022-03-05 13:26:22 -0500

    added date

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 # main executable file for workout tracker
 
+import datetime
 
-def create_workout(name: str, sets: int, reps: int):
+def create_workout(name: str, sets: int, reps: int, date: datetime.date = datetime.date.today(), num: int = 1):
     '''
     Method to create a workout
 
@@ -13,6 +14,10 @@ def create_workout(name: str, sets: int, reps: int):
         number of sets
     reps : int
         number of reps
+    date : datetime.date
+        date of workout
+    num : int
+        workout session number on single day
 
     Returns
     -------
@@ -25,19 +30,35 @@ def create_workout(name: str, sets: int, reps: int):
         If input variables are not the expected types (str, int, int)
     '''
 
-    if not isinstance(name, str):
+    try:
+        name = str(name)
+    except:
         raise TypeError("name is not a string")
     
-    if not isinstance(sets, int):
+    try:
+        sets = int(sets)
+    except:
         raise TypeError("sets is not an integer")
     
-    if not isinstance(reps, int):
+    try:
+        reps = int(reps)
+    except:
         raise TypeError("reps is not an integer")
     
+    if not isinstance(date, datetime.date):
+        raise TypeError("date is not a datetime.date object")
+    
+    try:
+        num = int(num)
+    except:
+        raise TypeError("num is not an integer")
+
     new_dict = {
         "name": name,
         "sets": sets,
-        "reps": reps
+        "reps": reps,
+        "date": date,
+        "num": num
     }
 
     return new_dict
@@ -57,9 +78,9 @@ def main():
             break
         
         if user_in == 1:
-            name = str(input("Input name of workout: "))
-            sets = int(input("Input number of sets: "))
-            reps = int(input("Input number of reps: "))
+            name = input("Input name of workout: ")
+            sets = input("Input number of sets: ")
+            reps = input("Input number of reps: ")
             workout_list.append(create_workout(name, sets, reps))
         
         if user_in == 2:
